fb061b687e45855ad5b573fba3a6f55dcd328cb7
ZerNurgul <zernurgul@gmail.com>
2021-12-25 09:36:08 +0000

    added more hello feature

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,5 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Devoloping hello feature");
+        System.out.println("Local repo is already publish to GitHub");
+        System.out.println("doing more work");
     }
 }
